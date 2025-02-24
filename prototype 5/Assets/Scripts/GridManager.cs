cd373b1762d79730e5cf519b1cf979fb2aa348f5
Devin Thorpe <devinthorpe77@gmail.com>
2025-02-24 14:16:42 -0800

    tiles1

--- prototype 5/Assets/Scripts/GridManager.cs
+++ prototype 5/Assets/Scripts/GridManager.cs
@@ -2,12 +2,13 @@ using Unity.Mathematics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
 
-    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Tile _groundTile,_keyTile;
 
     [SerializeField] private Transform _cam;
 
@@ -20,11 +21,11 @@ public class GridManager : MonoBehaviour
             _tiles = new Dictionary<Vector2, Tile>();
         for(int x = 0; x < _width;x++){
             for(int y = 0; y < _height;y++){
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y),Quaternion.identity);
+                var randomTile = Random.Range(0,6) == 3 ? _keyTile : _groundTile;
+                var spawnedTile = Instantiate(randomTile, new Vector3(x,y),Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
-                var isOffset = (x%2 ==0 && y %2 !=0 ) || (x%2 != 0 && y%2==0); 
-                spawnedTile.Init(isOffset);
+                spawnedTile.Init(x,y);
 
                 _tiles[new Vector2(x,y)] = spawnedTile;
             }
